70c5f23cdd35a898974fe9b031c532ae3711131b
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-06-05 22:28:34 +0300

    implement color manager

---
+++ lib/presentation/resources/color_manager.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color blue = Color(0xff005DB2);
+  static const Color darkBlue = Color(0xff0F2332);
+  static const Color lightBlue = Color(0xff1E6598);
+  static const Color babyBlue = Color(0xff7DB7E0);
+  static const Color lightBabyBlue = Color(0xff9DC8EC);
+  static const Color equalColor = Color(0xff1991FF);
+  static const Color grey = Color(0xff606061);
+  static const Color white = Color(0xffF7F8FB);
+}
