de34ca3a7b865f5b72807e15d8790efb76b1cbe7
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-06-05 22:35:22 +0300

    implement font manager

---
+++ lib/presentation/resources/fonts_manager.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+
+class FontFamilyManager {
+
+  static const String textFont = 'Merriweather';
+  static const String numberFont = 'Poppins';
+}
+
+class FontWeightManager
+{
+  static const FontWeight light = FontWeight.w500;
+  static const FontWeight regular = FontWeight.w600;
+  static const FontWeight medium = FontWeight.w700;
+  static const FontWeight semiBold = FontWeight.w800;
+  static const FontWeight bold = FontWeight.w900;
+}
+
+class FontSize
+{
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+  static const double s22 = 22.0;
+  static const double s25 = 25.0;
+  static const double s28 = 28.0;
+  static const double s30 = 30.0;
+  static const double s35 = 35.0;
+  static const double s40 = 40.0;
+  static const double s60 = 60.0;
+}
